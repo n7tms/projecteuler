2ad11fa390fd7ace37b2003c3e360e414ea71205
Todd Smith <tmsmith328@gmail.com>
2021-10-18 08:47:31 -0600

    working problem 23

---
+++ src/projecteuler/001-050/problem-023.clj
@@ -0,0 +1,81 @@
+;; Project Euler
+;; Problem 23: Non-abundant sums
+;; 
+
+;; A perfect number is a number for which the sum of its proper divisors is exactly 
+;; equal to the number. For example, the sum of the proper divisors of 28 would be 
+;; 1 + 2 + 4 + 7 + 14 = 28, which means that 28 is a perfect number.
+;; 
+;; A number n is called deficient if the sum of its proper divisors is less than n 
+;; and it is called abundant if this sum exceeds n.
+
+;; As 12 is the smallest abundant number, 1 + 2 + 3 + 4 + 6 = 16, the smallest number 
+;; that can be written as the sum of two abundant numbers is 24. By mathematical 
+;; analysis, it can be shown that all integers greater than 28123 can be written as 
+;; the sum of two abundant numbers. However, this upper limit cannot be reduced any 
+;; further by analysis even though it is known that the greatest number that cannot 
+;; be expressed as the sum of two abundant numbers is less than this limit.
+;; 
+;; Find the sum of all the positive integers which cannot be written as the sum of 
+;; two abundant numbers.
+;; 
+
+(ns projecteuler)
+
+;; produce a list of divisors of n, not including n
+;; e.g. (divisors 10) => (1 2 5)
+(defn divisors
+  ""
+  [n]
+  (filter (comp zero? (partial rem n)) (range 1 n)))
+
+
+
+(defn abundant? [num]
+  "return true if the num is an abundant number (sum of the divisors is greater than the number"
+  (if (< num (reduce + (divisors num)))
+    true
+    false))
+
+(def abundants
+  "generate a sequence of abundant numbers, 12-28123"
+  (remove nil?
+          (for [x (range 12 28123)]
+            (if (abundant? x)
+              x)
+            )))
+
+;;(count abundants)   ;; => 6965
+
+;; iterate through pairs of abundants looking for two that sum to the given num
+;; return true if a pair is found.
+(defn sum-of-abundants? [num]
+  (let [x take-while (partial > num) abundants
+        y take-while (partial > num) abundants]
+    
+    )
+
+  )
+  
+(reduce + ([1 2 3] [1 2 3]))
+
+
+(take-while (partial > 100 ) abundants)
+
+
+;; (let a [create a list of all abundant numbers < 28123]
+;; (let [x range 1 28123]
+;; check combinations of a1 + a2 where a < x
+;; ... if a combination is found, then move on.
+;; ... if one is not found, then remember this number
+
+
+
+
+
+
+
+
+
+
+
