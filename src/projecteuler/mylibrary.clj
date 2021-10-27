b443a3a3c2b82699e4d65daf93c7418fc0e52718
Todd Smith <tmsmith328@gmail.com>
2021-10-27 17:09:21 -0600

    problem 045 solution

--- src/projecteuler/mylibrary.clj
+++ src/projecteuler/mylibrary.clj
@@ -183,10 +183,46 @@
   (map (fn [x] (int (* 0.5 x (inc x)))) (range))
   )
 
+(defn triangle? [value]
+  (if (< value 0)
+    false
+    (let [c (* -2 value)
+          b 1
+          a 1
+          d (- (* b b) (* 4 a c))]
+      (if (< d 0)
+        false
+        (let [root1 (/ (+ (- b) (Math/sqrt d)) (* 2 a))
+              root2 (/ (- (- b) (Math/sqrt d)) (* 2 a))]
+          (if (and (> root1 0)
+                   (= root1 (Math/floor root1)))
+            true
+            (if (and (> root2 0)
+                     (= root2 (Math/floor root2)))
+              true
+              false))))))
+  )
+
 
 (def pentagonal-numbers
   "lazy-sequence of pentagonal numbers"
-  (map (fn [x] (int (* x (- (* 3 x) 1) 0.5))) (range)))
+  (map (fn [x] (/ (* x (- (* 3 x) 1)) 2)) (iterate inc 1)))
+
+(defn pentagonal? [value]
+  (zero? (rem (+ 1 (Math/sqrt (+ 1 (* 24 value)))) 6)))
+
+
+(def hexagonal-numbers
+  "laxy-sequence of hexagonal numbers"
+  (map (fn [x] (* x (- (* 2 x) 1))) (iterate inc 1)))
+
+(defn hexagonal? [value]
+  (let [n (/ (+ (Math/sqrt (+ (* 8 value) 1)) 1) 4)]
+    (if (= n (Math/floor n))
+      true
+      false))
+  )
+
 
 (defn abs [n]
   "given a number n, return the absolute value of n"
