85a929c64d63de4e082a1d1a6259cea5a1b356f2
Todd Smith <n7tms@yahoo.com>
2021-11-08 16:08:09 -0700

    problem 38 solution

--- src/projecteuler/001-050/problem-038.clj
+++ src/projecteuler/001-050/problem-038.clj
@@ -1,6 +1,21 @@
 ;; Project Euler
 ;; Problem 038 - Pandigital Multiples
 ;;
+(ns projecteuler)
+
+
+(defn to-digits [num]
+  (map #(Character/getNumericValue %) (str num)))
+
+(defn unique-digits? [digits]
+  (let [unique (distinct digits)]
+    (= (count unique) (count digits))))
+
+(defn pandigital? [digits]
+  "returns true if a seq of digits is pandigital-9"
+  (and (nil? (some zero? digits))
+       (= 9 (count digits))
+       (unique-digits? digits)))
 
 ;; concat nx1 and nx2
 ;; A: if length > 9, then next n
@@ -12,7 +27,7 @@
 ;; sort the resulting list or take the max
 ;;    
 
-;; loop n from 2 to (987654321 / 2) + 1
+;; loop n from 2 to 50000 
 ;;   let result = n
 ;;   loop m from 2 to 9 (can't be longer than 9 digits)
 ;;     concat result and n times m
@@ -24,4 +39,19 @@
 ;;           loop m
 ;;           loop n
 
- 
+(defn problem-038 []
+  (apply max
+         (map #(Integer/parseInt %)
+              (remove nil?
+                      (for [n (range 2 50000)]
+                        (loop [result (str n)
+                               mult 1]
+                          (if (< (count (str result)) 10)
+                            (if (pandigital? (to-digits result))
+                              result
+                              (recur (str result (* n (inc mult))) (inc mult)) ))))))))
+
+(problem-038)   ;; => 932718654
+
+
+
