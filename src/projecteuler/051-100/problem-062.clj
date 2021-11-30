261d7f056f001440c750ccb13eba4b220486e656
Todd Smith <n7tms@yahoo.com>
2021-11-29 17:02:49 -0700

    working problem 62

--- src/projecteuler/051-100/problem-062.clj
+++ src/projecteuler/051-100/problem-062.clj
@@ -1,7 +1,11 @@
 ;; Project Euler
 ;; Problem 62 - Cubic Permutations
 ;;
-
+;; The cube, 41063625 (3453), can be permuted to produce two other cubes: 
+;; 56623104 (3843) and 66430125 (4053). In fact, 41063625 is the smallest 
+;; cube which has exactly three permutations of its digits which are also cube.
+;;
+;;Find the smallest cube for which exactly five permutations of its digits are cube.
 
 (ns projecteuler
   (:require [clojure.math.combinatorics :as combo]))
@@ -9,15 +13,40 @@
 (defn to-digits [num]
   (map #(Character/getNumericValue %) (str num)))
 
+(def cubes (map #(* % % %) (range 346 10000)))
 
-(combo/permutations (to-digits 123))
+;; generate a long list of cubes
+;; "for" each cube in the list
+;;    generate all the permutations
+;;    map - take the cube-root of each permutation
+;;    filter out the ones that are not perfect roots
+;;    see if there are 5 cube roots remaining
+;;    if so, that's our man!
 
 
-(for [x '(344 345 346)]
-  x)
+(defn digits-to-int [x]
+  "given: '(1 2 3), return: 123"
+  (Integer/parseInt (apply str  x))
+)
 
-(Math/cbrt 8)
 
-(def x 345)
-(= (float  x) (Math/cbrt 41063625))
-(= (float x) 345.000000000000000001)
+(defn problem-062 []
+  (remove nil? 
+          (for [x cubes]
+            (if (= 5 (count
+                      (remove #(not= (float %) %)
+                              (map #(Math/cbrt %)
+                                   (map #(digits-to-int %)
+                                        (combo/permutations (to-digits x)))))))
+              x)
+            )))
+
+(time (problem-062))
+
+
+
+
+
+
+
+
